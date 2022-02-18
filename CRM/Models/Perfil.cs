06fdc83314b3f61c788503b791286016d229dbcd
Elias <eliasmerc23@fpuna.edu.py>
2022-02-18 20:50:29 -0300

    Se agregan mappings necesarios.

---
+++ CRM/Models/Perfil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CRM.Models
+{
+    public partial class Perfil
+    {
+        public Perfil()
+        {
+            Usuarios = new HashSet<Usuario>();
+        }
+
+        public int IdPerfil { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
+
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ CRM/Models/PerfilPermiso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CRM.Models
+{
+    public partial class PerfilPermiso
+    {
+        public int IdPerfilPermiso { get; set; }
+        public int IdPerfil { get; set; }
+        public int IdMenu { get; set; }
+
+        public virtual Menu IdMenuNavigation { get; set; }
+        public virtual Perfil IdPerfilNavigation { get; set; }
+    }
+}
