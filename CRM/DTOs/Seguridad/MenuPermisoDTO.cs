d26100013a2732f36608aa8eecd6c64fb288b3ff
Elías Mercado <eliasmerc23@gmail.com>
2022-11-09 12:59:00 -0300

    Administracion de permisos.

---
+++ CRM/DTOs/Seguridad/MenuPermisoDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.DTOs.Seguridad
+{
+    public class MenuPermisoDTO
+    {
+        public int IdMenu { get; set; }
+        public int IdMenuPadre { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool TienePermiso { get; set; }
+    }
+
+    public class DatoPermisoDTO
+    {
+        public int IdPerfil { get; set; }
+        public int IdMenu { get; set; }
+        public bool ActivarMenu { get; set; }
+    }
+
+}
